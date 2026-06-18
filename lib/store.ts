b224d5fb03bd090deb2ff1364171e0ed9586dc21
interspecular <interspecular@gmail.com>
2026-06-18 20:23:12 +0500

    [studio] Dialogue UI: Added second portrait to tavern demo for multiple portraits example.

--- lib/store.ts
+++ lib/store.ts
@@ -759,6 +759,7 @@ const createDefaultPages = (): StudioPage[] => [
     uiWidgets: [
       { id: 'w_dlg1', type: 'dialogueBox', layout: { x: 16, y: 78, width: 68, height: 12, z: 20 }, style: 'default', data: { textSource: 'page' } },
       { id: 'w_port1', type: 'portrait', layout: { x: 75, y: 30, width: 20, height: 40, z: 5 }, data: { speakerId: 'mila' } },
+      { id: 'w_port2', type: 'portrait', layout: { x: 2, y: 30, width: 20, height: 40, z: 5 }, data: { speakerId: 'slay' } },
       { id: 'w_q1', type: 'quickAction', layout: { x: 2, y: 15, width: 7, height: 7, z: 30 }, data: { actionType: 'inventory' } },
     ],
     uiLayoutPreset: 'classic_vn',
